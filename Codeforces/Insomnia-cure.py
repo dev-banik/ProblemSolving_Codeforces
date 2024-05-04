1e99b1e8ed6c7b4e9b1e7b6089d40e2f2a93a935
dev-banik <dev.banik401@gmail.com>
2024-05-04 13:44:36 +0600

    codeforces-148A-insomniacure

---
+++ Codeforces/Insomnia-cure.py
@@ -0,0 +1,10 @@
+count=0
+k=int(input())
+l=int(input())
+m=int(input())
+n=int(input())
+d=int(input())
+for i in range(1, d+1):
+    if i%k==0 or i%l==0 or i%m==0 or i%n==0:
+        count+=1
+print(count)
